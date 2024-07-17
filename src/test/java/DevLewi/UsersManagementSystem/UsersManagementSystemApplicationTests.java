b66375988504eee0e67573a0515c3ffc758ddc1a
lewis wanjohi <wanjohilewi7@gmail.com>
2024-07-17 16:00:46 +0300

    implementation of spring security and JWT

---
+++ src/test/java/DevLewi/UsersManagementSystem/UsersManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package DevLewi.UsersManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
