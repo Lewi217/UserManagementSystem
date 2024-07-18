feb32dfa4fc0fa1a65f95befc280c6d71abb167f
lewis wanjohi <wanjohilewi7@gmail.com>
2024-07-18 15:04:08 +0300

    implementation of spring security and JWT

---
+++ src/main/java/DevLewi/UsersManagementSystem/repository/UsersRepo.java
@@ -0,0 +1,11 @@
+package DevLewi.UsersManagementSystem.repository;
+
+import DevLewi.UsersManagementSystem.entity.OurUsers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepo extends JpaRepository<OurUsers, Integer> {
+
+    Optional<OurUsers> findByEmail(String email);
+}
