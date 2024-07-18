31eaa31b0a94afad19e61e2ee46b85ca7f1b235f
lewis wanjohi <wanjohilewi7@gmail.com>
2024-07-19 01:21:30 +0300

    implementation of spring security and JWT

--- src/main/java/DevLewi/UsersManagementSystem/service/UsersManagementService.java
+++ src/main/java/DevLewi/UsersManagementSystem/service/UsersManagementService.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserManagementService {
+public class UsersManagementService {
 
     @Autowired
     private UsersRepo usersRepo;
