37c2ecce5b6f2f09b96ee3e4b9cb5b4c329e8c04
wanjohi lewis <wanjohilewi7@gmail.com>
2024-07-22 23:46:13 +0300

    implementation of spring security and JWT

--- src/main/java/DevLewi/UsersManagementSystem/controller/UserManagementController.java
+++ src/main/java/DevLewi/UsersManagementSystem/controller/UserManagementController.java
@@ -16,7 +16,7 @@ public class UserManagementController {
     private UsersManagementService usersManagementService;
 
     @PostMapping("/auth/register")
-    public ResponseEntity<ReqRes> regeister(@RequestBody ReqRes reg){
+    public ResponseEntity<ReqRes> register(@RequestBody ReqRes reg){
         return ResponseEntity.ok(usersManagementService.register(reg));
     }
 
